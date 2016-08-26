f0966e3f2584fa1d36e3d46f4b4d8c7e15376acf
JustTundra <katanahyru@gmail.com>
2016-08-26 16:34:57 +1200

    Altered some code on route to character create screen. No longer 500ing but now 404ing. Removed play.ejs as it was in the wrong folder and the wrong filetype; replaced with game.js

--- routes/index.js
+++ routes/index.js
@@ -12,27 +12,5 @@ router.get('/', function(req, res, next) {
 	res.render('index', pageData);
 });
 
-/* GET character create page */
-router.get('/create', function(req, res, next) {
-	var pageData = commonRoute.initPageData(req.session);
-	//Add the map class to html body
-	pageData.bodyClass = 'map';
-	res.render('create'. pageData);
-});
-
-/*POST character create page. */
-router.post('/create', function (req, res, next) {
-	var pageData = commonRoute.initPageData(req.session);
-	pageData.bodyClass = 'map';
-	var postedForm =req.body;
-	if (postedForm.Name == null){
-		pageData.errorMsg += "Invalid name." ;
-		res.render('create', pageData);
-	} else {
-		//Do something with character details
-		pageData.outputText = 'Welcome to Polemios, '+postedForm.Name +"!";
-		res.render('create', pageData);
-		}
-});
 
 module.exports = router;
