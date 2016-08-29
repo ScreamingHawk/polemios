2b8461966ecca2c73acc0601344bba2b416c6f80
Michael Standen <screaminghawk@gmail.com>
2016-08-29 19:53:41 +1200

    Added more static data

--- gameData.js
+++ gameData.js
@@ -1,31 +1,40 @@
 /* Get static look up values */
 var db = require('./db');
 
-module.exports.updateRaces = function(){
+module.exports.updateRaces = function(next){
 	db.runSql('SELECT raceId, name, description FROM race', [], function(dbRaces){
 		module.exports.races = dbRaces;
 		console.log("Races: " + JSON.stringify(dbRaces, null, 2));
+		if (next){
+			next(dbRaces);
+		}
 	});
 };
 module.exports.updateRaces();
 
-module.exports.updateFactions = function(){
-	db.runSql('SELECT factionId, name, description FROM faction', [], function(dbFactions){
+module.exports.updateFactions = function(next){
+	db.runSql('SELECT factionId, name, description, startingMapId FROM faction', [], function(dbFactions){
 		module.exports.factions = dbFactions;
 		console.log("Factions: " + JSON.stringify(dbFactions, null, 2));
+		if (next){
+			next(dbFactions);
+		}
 	});
 };
 module.exports.updateFactions();
 
-module.exports.updateMaps = function(){
+module.exports.updateMaps = function(next){
 	db.runSql('SELECT mapId, name, description, height, width, pvp FROM map', [], function(dbMaps){
 		module.exports.maps = dbMaps;
 		console.log("Maps: " + JSON.stringify(dbMaps, null, 2));
+		if (next){
+			next(dbMaps);
+		}
 	});
 };
 module.exports.updateMaps();
 
-module.exports.updateMapLocations = function(mapId){
+module.exports.updateMapLocations = function(mapId, next){
 	db.runSql('SELECT locationId, x, y, locationType FROM location WHERE mapId = ?', [mapId], function(dbLocations){
 		module.exports.maps.forEach(function(map){
 			if (map.mapId == mapId){
@@ -33,5 +42,18 @@ module.exports.updateMapLocations = function(mapId){
 			}
 		});
 		console.log("Locations for map("+mapId+"): " + JSON.stringify(dbLocations, null, 2));
+		if (next){
+			next(dbLocations);
+		}
 	});
-}
+};
+
+module.exports.updateRaceFactionDefaults = function(next){
+	db.runSql('SELECT raceFactionDefaultId, raceId, factionId, fame FROM raceFactionDefault', [], function(dbRaceFactionDefaults){
+		module.exports.raceFactionDefaults = dbRaceFactionDefaults;
+		console.log("Race Faction Defaults: " + JSON.stringify(dbRaceFactionDefaults, null, 2));
+		if (next){
+			next(dbRaceFactionDefaults);
+		}
+	});
+};
